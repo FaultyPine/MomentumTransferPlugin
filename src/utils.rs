593404275978642384d457980d0043920bb28b58
PiNE <53064235+FaultyPine@users.noreply.github.com>
2020-05-30 23:42:50 -0500

    initial commit
    bruh

---
+++ src/utils.rs
@@ -0,0 +1,15 @@
+
+use smash::lib::{self, lua_const::*};
+use smash::app::{self, lua_bind::*};
+
+
+pub fn get_category(boma: &mut app::BattleObjectModuleAccessor) -> i32{
+    return (boma.info >> 28) as u8 as i32;
+}
+extern "C"{
+    #[link_name = "\u{1}_ZN3app7utility8get_kindEPKNS_26BattleObjectModuleAccessorE"]
+    pub fn get_kind(module_accessor: &mut app::BattleObjectModuleAccessor) -> i32;
+}
+pub unsafe fn get_player_number(boma: &mut app::BattleObjectModuleAccessor) -> usize{
+    app::lua_bind::WorkModule::get_int(boma, *FIGHTER_INSTANCE_WORK_ID_INT_ENTRY_ID) as usize
+}
